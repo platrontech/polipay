f56edc010c20ceb5a2992bc04e06aa53ae72d33d
omertaskin <omertaskin@netaxtech.com>
2020-12-24 23:31:58 +0300

    json error was fixed

--- src/main/java/com/polinity/polipay/commons/api/model/DoneResponse.java
+++ src/main/java/com/polinity/polipay/commons/api/model/DoneResponse.java
@@ -1,12 +1,16 @@
 package com.polinity.polipay.commons.api.model;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Value;
 
-@Value
+@Data
 @Builder
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class DoneResponse extends BaseResponse {
 
   public static DoneResponse of() {
